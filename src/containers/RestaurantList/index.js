07d64cadf58569a56c332b4fad6abb51945dcc3f
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-20 13:41:02 +0100

    adds RestaurantList component

---
+++ src/containers/RestaurantList/index.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import Restaurant from '../../components/Restaurant';
+
+const RestaurantList = (props) => {
+  const {
+    restaurantData,
+  } = props;
+
+  const renderedItems = (restaurantData).map((restaurant, index) => (
+    <Fragment key={restaurantData._id}>
+      <Restaurant restaurant={restaurant} />
+    </Fragment>
+  ));
+
+  return (
+    <div>
+      {renderedItems}
+    </div>
+  );
+};
+
+export default RestaurantList;
