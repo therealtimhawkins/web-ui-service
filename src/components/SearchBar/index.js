c49b086b430e0a70007155cca18e44268d056935
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-26 16:20:10 +0100

    adds placeholder text to search input

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,7 +3,7 @@ import './SearchBar.css';
 
 const SearchBar = (props) => (
   <div className="searchBar">
-    <input className="searchBarInput" onChange={props.onChange}></input>
+    <input className="searchBarInput" type="text" placeholder="e.g. E14 7DX" onChange={props.onChange}></input>
     <button className="searchBarButton" onClick={props.onClick} >Find</button>
   </div>
 );
