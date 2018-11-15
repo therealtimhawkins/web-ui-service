e4adfb112e2683f16721eab35634a3d266ee3241
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-11-15 21:58:49 +0000

    style updates

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Logo.css';
 
 const Logo = () => (
-  <div>
+  <div className="logo" >
     <img src="images/logo/logoVegitableOrange.svg" className="logoVegitable" alt="logoVegitable" />
   </div>
 );
