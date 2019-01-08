315cb4b95036c1851b02e9855356e79a4d8f5302
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-08 19:55:51 +0000

    adds state to Registration form

--- src/containers/UserContainers/RegistrationForm/index.js
+++ src/containers/UserContainers/RegistrationForm/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const RegistrationForm = () => {
-  return (
-    <div>
-      Testing!
-    </div>
-  )
+class RegistrationForm extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+  
+  updateStateParameter = (e) => {
+    const param = e.target.className;
+    this.setState({
+      [param]: e.target.value
+    });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <input className="firstName" type="text" placeholder="first name" 
+          onChange={this.updateStateParameter}></input>
+        <input className="lastName" type="text" placeholder="last name" 
+          onChange={this.updateStateParameter}></input>
+        <input className="email" type="text" placeholder="email" 
+          onChange={this.updateStateParameter}></input>
+        <input className="password" type="text" placeholder="password" 
+          onChange={this.updateStateParameter}></input>
+      </div>
+    )
+  }
 }
 
 export default RegistrationForm;
