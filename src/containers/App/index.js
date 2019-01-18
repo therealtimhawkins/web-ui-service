5c5b101581280f1c632a5a2a563259dfa6dd48b2
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-18 15:33:57 +0000

    refactors App.js to be stateless

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -7,43 +7,22 @@ import RestaurantContainer from '../RestaurantsContainers/RestaurantContainer';
 import RegistrationForm from '../UserContainers/RegistrationForm';
 import './App.css';
 
-class App extends Component {
-  state = {
-    postcode: '',
-  };
-
-  refreshHomepage = () => {
-    this.setState({
-      postcode: '',
-      restaurantData: null,
-    });
-  };
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <SearchBar />
-          <Route 
-            path='/restaurants' 
-            render={(routeProps) => (
-              <RestaurantContainer restaurantData={this.state.restaurantData} postcode={this.state.postcode}/> 
-            )} 
-          />
-          <Route path='/user' component={RegistrationForm} />
-        </div>
-      </Router>
-    );
-  };
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <SearchBar />
+        <Route 
+          path='/restaurants' 
+          render={(routeProps) => (
+            <RestaurantContainer /> 
+          )} 
+        />
+        <Route path='/user' component={RegistrationForm} />
+      </div>
+    </Router>
+  );
 };
 
-const mapStateToProps = state => ({
-  restaurantData: state.restaurantData,
-});
-
-const ConnectedApp = connect(
-  mapStateToProps,
-)(App);
-
-export default ConnectedApp;
+export default App;
