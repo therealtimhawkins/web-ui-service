c54031a96a25795ebfc3da9b51a130f24065299f
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-20 15:09:52 +0000

    adds null route for userprofile page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import history from '../../history';
 import Header from '../Header';
 import SearchBar from '../../components/SearchBar';
@@ -9,16 +10,19 @@ import RegistrationForm from '../UserContainers/RegistrationForm';
 import LoginForm from '../UserContainers/LoginForm';
 import './App.css';
 
-const App = () => {
+const App = (props) => {
   return (
     <Router history={history}>
       <div className="App">
         <Header />
         <SearchBar />
-        <Route 
-          path='/user/profile'
-          component={UserProfile}
-        />
+        { props.user ? 
+          <Route 
+            path='/user/profile'
+            component={UserProfile}
+          />
+          : null  
+        }
         <Route 
           path='/restaurants' 
           render={(routeProps) => (
@@ -32,4 +36,17 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const mapDispatchToProps = {
+
+}
+
+const ConnectedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default ConnectedApp;
