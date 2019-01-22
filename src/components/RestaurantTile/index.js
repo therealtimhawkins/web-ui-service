86e4c96719770be4cbf410c72c19b55f4c8e7678
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-22 16:49:48 +0000

    adds saveRestaurant functionality to restaurantTile

--- src/components/RestaurantTile/index.js
+++ src/components/RestaurantTile/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import DishList from '../../containers/RestaurantsContainers/DishesList';
 import RestaurantInfo from '../RestaurantInfo';
 import './RestaurantTile.css';
@@ -25,12 +26,49 @@ class RestaurantTile extends Component {
     console.log(this.state.vegetarianClicked);
   };
 
+  saveButtonClicked = async () => {
+    let auth = await this.authUser();
+    if (auth) {
+      let result = await this.saveRestaurant();
+      console.log(result);
+    }
+    console.log(auth);
+  }
+
+  authUser = async () => {
+    const apiAuthEndpoint = process.env.REACT_APP_USER_DATA_API_URL + 'auth';
+    try {
+      let result = await axios.get(apiAuthEndpoint, { headers: { 'x-access-token': this.props.user.token }});
+      return result;
+    } catch {
+      return null;
+    }
+  }
+
+  saveRestaurant = () => {
+    const apiSaveRestaurantEndpoint = process.env.REACT_APP_PROFILE_API_URL + 'saveRestaurant';
+    try {
+      let result = axios.post(apiSaveRestaurantEndpoint, {
+        userEmail: this.props.user.email,
+        restaurants: [
+          {
+            name: this.props.restaurantData.name,
+            postcode: this.props.restaurantData.postcode
+          }
+        ]
+      });
+      return result;
+    } catch {
+      return null;
+    }
+  }
+
   render() {
     return (
       <div className="restaurant">
         <div className="restaurantName" >{this.props.restaurantData.name}</div>
         { this.props.user ? 
-          <button onClick={() => console.log('button working')}>Save</button> 
+          <button onClick={this.saveButtonClicked}>Save</button> 
           : null
         }
 
